28301babfcfbac9987b9071775d5e8385fc394d5
yuwanfeng <1950735817@qq.com>
2017-07-30 02:23:40 +0800

    add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
-{
-  "rules": {
-
-  }
-}
-
-
-
-
 // {
 //   "rules": {
 //   "semi": ["error", "always"],
@@ -17,3 +8,16 @@
 // "off" or 0 - turn the rule off
 // "warn" or 1 - turn the rule on as a warning (doesn't affect exit code)
 // "error" or 2 - turn the rule on as an error (exit code will be 1)
+
+
+{
+	"parserOptions": {
+		"ecmaVersion": 6,
+		"ecmaFeatures": {
+			"experimentalObjectRestSpread": true
+		}
+	},
+	"parser": "babel-eslint"
+}
+
+
