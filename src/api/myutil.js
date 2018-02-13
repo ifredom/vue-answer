d1f76b1b66f44a46cba5ad6a2ef52d3f5fc753a5
ifredom <1950735817@qq.com>
2018-02-13 10:42:16 +0800

    优化，防止多次ajax请求

--- src/api/myutil.js
+++ src/api/myutil.js
@@ -4,64 +4,72 @@ import Qs from 'qs';
 import { Toast } from 'mint-ui';
 
 export const formatFileParams = data => {
-  var formData = new FormData(); // FormData 对象
-  var params = {};
+    var formData = new FormData(); // FormData 对象
+    var params = {};
 
-  if (data&&!data.hasOwnProperty('values')) {
-    params['values'] = Object.assign({}, data);
-  } else {
-    params['values'] = Object.assign({}, data.values);
-  }
-
-  for (let item in params) {
-    if (item === 'values') {
-      // 普通参数
-      for (let name in params[item]) {
-        formData.append(name, params[item][name]);
-      }
-    } else if (item === 'files') {
-      // 文件参数
-      for (let name in params[item]) {
-        let fileList = params[item][name];
-        for (let i = 0; i < fileList.length; i++) {
-          formData.append(name, fileList[i]);
-        }
-      }
+    if (data && !data.hasOwnProperty('values')) {
+        params['values'] = Object.assign({}, data);
+    } else {
+        params['values'] = Object.assign({}, data.values);
     }
-  }
-  return formData;
-};
 
-export default function formMiddlePromise(url, params={}, method = 'post') {
-  params = formatFileParams(params);
-  method = method.toUpperCase();
-  return new Promise((resolve, reject) => {
-    NProgress.start();
-    axios({
-      method: method,
-      url: url,
-      timeout: 10000,
-      data: params,
-      // transformRequest: [(data, headers) => Qs.stringify(data)],
-      headers: {
-        'Content-Type': 'text/html; charset=UTF-8'
-      }
-    })
-      .then(
-        response => {
-          NProgress.done();
-          resolve(response.data);
-        },
-        err => {
-          NProgress.done();
-          Toast(err);
-          Toast('服务器异常，请稍后再试');
-          reject(err);
+    for (let item in params) {
+        if (item === 'values') {
+            // 普通参数
+            for (let name in params[item]) {
+                formData.append(name, params[item][name]);
+            }
+        } else if (item === 'files') {
+            // 文件参数
+            for (let name in params[item]) {
+                let fileList = params[item][name];
+                for (let i = 0; i < fileList.length; i++) {
+                    formData.append(name, fileList[i]);
+                }
+            }
         }
-      )
-      .catch(thrown => {
-        NProgress.done();
-        Toast(thrown);
-      });
-  });
+    }
+    return formData;
+};
+// 防止多次ajax请求
+const requesting = {};
+export default function formMiddlePromise(url, params = {}, method = 'post') {
+    params = formatFileParams(params);
+    method = method.toUpperCase();
+    let uid = url + method + (!!~+[] + {});
+    if (requesting[uid]) {
+        console.warn('请勿重复点击');
+        return;
+    }
+    requesting[uid] = true;
+
+    return new Promise((resolve, reject) => {
+        NProgress.start();
+        axios({
+            method: method,
+            url: url,
+            timeout: 10000,
+            data: params,
+            // transformRequest: [(data, headers) => Qs.stringify(data)],
+            headers: {
+                'Content-Type': 'text/html; charset=UTF-8'
+            }
+        })
+            .then(
+                response => {
+                    NProgress.done();
+                    resolve(response.data);
+                    requesting[uid] = false;
+                },
+                err => {
+                    NProgress.done();
+                    Toast(err);
+                    reject(err);
+                }
+            )
+            .catch(thrown => {
+                NProgress.done();
+                Toast(thrown);
+            });
+    });
 }
